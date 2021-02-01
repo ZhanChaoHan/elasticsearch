bc3458e0d7f59f20caf4d669fd74c27a8146c598
jachs <799516423@qq.com>
2021-02-01 10:34:22 +0800

    get all index

--- elasticsearch/src/test/java/com/jachs/elasticsearch/boot/rest/ElasticsearchRestTemplateIndex.java
+++ elasticsearch/src/test/java/com/jachs/elasticsearch/boot/rest/ElasticsearchRestTemplateIndex.java
@@ -1,10 +1,15 @@
 package com.jachs.elasticsearch.boot.rest;
 
 import java.io.IOException;
+import java.util.Map;
+import java.util.Set;
 
+import org.elasticsearch.action.admin.indices.alias.get.GetAliasesRequest;
+import org.elasticsearch.client.GetAliasesResponse;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.client.indices.CreateIndexRequest;
+import org.elasticsearch.cluster.metadata.AliasMetadata;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -26,5 +31,16 @@ public class ElasticsearchRestTemplateIndex {
 	public void test1() throws IOException {
 		CreateIndexRequest cir=new CreateIndexRequest("user");
 		elasticsearchClient.indices().create(cir, RequestOptions.DEFAULT);
+	}
+	//获取全部索引
+	@Test
+	public void test2() throws IOException {
+	    GetAliasesRequest request = new GetAliasesRequest();
+        GetAliasesResponse getAliasesResponse =  elasticsearchClient.indices().getAlias(request,RequestOptions.DEFAULT);
+        Map<String, Set<AliasMetadata>> map = getAliasesResponse.getAliases();
+        Set<String> indices = map.keySet();
+        for (String key : indices) {
+            System.out.println(key);
+        }
 	}
 }
