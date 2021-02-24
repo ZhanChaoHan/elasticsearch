b44af9ea1ea7c70ab0121b57c0d49366306dd9d2
jachs <799516423@qq.com>
2021-02-24 17:20:13 +0800

    数据类型

--- elasticsearch/src/test/java/com/jachs/elasticsearch/boot/rest/query/ElasticsearchRestTemplateSimpleQueryTest.java
+++ elasticsearch/src/test/java/com/jachs/elasticsearch/boot/rest/query/ElasticsearchRestTemplateSimpleQueryTest.java
@@ -61,8 +61,43 @@ public class ElasticsearchRestTemplateSimpleQueryTest {
 		 printSearchHit(searchHit);
 		 System.out.println(searchHit.length);
 	}
-	//区间查询
-	//gt>,lt<;gte>=,lte<=
+	/****
+	 * 区间查询</br>
+	 * gt>,lt<;gte>=,lte<=</br>
+	 * 1. 核心数据类型</br>
+                    （1）字符串类型： text, keyword</br>
+                    （2）数字类型：long, integer, short, byte, double, float, half_float, scaled_float</br>
+                    （3）日期：date</br>
+                    （4）日期 纳秒：date_nanos</br>
+                    （5）布尔型：boolean</br>
+                    （6）Binary：binary</br>
+                    （7）Range: integer_range, float_range, long_range, double_range, date_range</br>
+       2. 复杂数据类型</br>
+                    （1）Object: object(for single JSON objects)</br>
+                    （2）Nested: nested (for arrays of JSON objects)</br>
+       3. 地理数据类型</br>
+                    （1）Geo-point： geo_point （for lat/lon points）</br>
+                    （2）Geo-shape: geo_shape (for complex shapes like polygons)</br>
+       4. 特殊数据类型</br>
+                    （1）IP:  ip (IPv4 和 IPv6 地址)</br>
+                    （2）Completion类型：completion （to provide auto-complete suggestions）</br>
+                    （3）Token count：token_count (to count the number of tokens in a string)</br>
+                    （4）mapper-murmur3：murmur3(to compute hashes of values at index-time and store them in the index)</br>
+                    （5）mapper-annotated-text：annotated-text （to index text containing special markup (typically used for identifying named entities)）</br>
+                    （6）Percolator：（Accepts queries from the query-dsl）</br>
+                    （7）Join：（Defines parent/child relation for documents within the same index）</br>
+                    （8）Alias：（Defines an alias to an existing field.）</br>
+                    （9）Rank feature：（Record numeric feature to boost hits at query time.）</br>
+                    （10）Rank features：（Record numeric features to boost hits at query time.）</br>
+                    （11）Dense vector：（Record dense vectors of float values.）</br>
+                    （12）Sparse vector：（Record sparse vectors of float values.）</br>
+                    （13）Search-as-you-type：（A text-like field optimized for queries to implement as-you-type completion）</br>
+       5.数组类型</br>
+                            在Elasticsearch中，数组不需要一个特定的数据类型，任何字段都默认可以包含一个或多个值，当然，这多个值都必须是字段指定的数据类型。</br>
+                    6.Multi-fields</br>
+                    Multi-fields 通常用来以不同的方式或目的索引同一个字段。比如，一个字符串类型字段可以同时被映射为 text 类型以用于全文检索、 keyword字段用于排序或聚合。又或者，你可以用standard分析器、english分析器和french分析器来索引同一个 text字段。
+	 * @author zhanchaohan
+	 */
 	@Test
 	public void test2() throws IOException {
 		 rq.source(searchSourceBuilder);
@@ -73,16 +108,21 @@ public class ElasticsearchRestTemplateSimpleQueryTest {
 //		 searchSourceBuilder.query(QueryBuilders.rangeQuery("userAge").gt(15).lt(20));
 //		 searchSourceBuilder.query(QueryBuilders.rangeQuery("userAge").gte(15).lte(20));
 		 
+//		 searchSourceBuilder.query(QueryBuilders.rangeQuery("userMoney").gte(new Float ( 0.6046738164042479 )).lte(new Float ( 0.8046738164042479 )));
+//		 searchSourceBuilder.query(QueryBuilders.rangeQuery("userMoney").gte(0.6046738164042479).lte(0.8046738164042479));
 		 
 		 //时间区间查询
-		 searchSourceBuilder.query(QueryBuilders.rangeQuery("timestamp1").from ( 20210129174011L ).to ( 20210229174011L ));
+//		 searchSourceBuilder.query(QueryBuilders.rangeQuery("timestamp1").from ( 14870224164535L ).to ( 14870224174535L ));
+		 
+//		 searchSourceBuilder.query(QueryBuilders.rangeQuery("mtime.keyword").gte ( "1520-03-06T08:45:35.200Z" )
+//		         .lte ( "1530-03-06T08:45:35.200Z" ));
 		 
 		 
 		 SearchResponse srr=elasticsearchClient.search(rq, RequestOptions.DEFAULT);
 		 
 		 SearchHit[] searchHit=srr.getHits().getHits();
-		 printSearchHit(searchHit);
 		 System.out.println(searchHit.length);
+		 printSearchHit(searchHit);
 	}
 	//简单查询
 	@Test
