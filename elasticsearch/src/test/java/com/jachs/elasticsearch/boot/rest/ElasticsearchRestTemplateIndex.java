821b69e791aa5f1bbbfd1f107149eceabfef2b19
jachs <799516423@qq.com>
2021-02-25 14:01:45 +0800

    删除全量索引

--- elasticsearch/src/test/java/com/jachs/elasticsearch/boot/rest/ElasticsearchRestTemplateIndex.java
+++ elasticsearch/src/test/java/com/jachs/elasticsearch/boot/rest/ElasticsearchRestTemplateIndex.java
@@ -5,6 +5,8 @@ import java.util.Map;
 import java.util.Set;
 
 import org.elasticsearch.action.admin.indices.alias.get.GetAliasesRequest;
+import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
+import org.elasticsearch.action.support.master.AcknowledgedResponse;
 import org.elasticsearch.client.GetAliasesResponse;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
@@ -43,4 +45,24 @@ public class ElasticsearchRestTemplateIndex {
             System.out.println(key);
         }
 	}
+	//单索引删除
+	@Test
+	public void test3() throws IOException {
+	    AcknowledgedResponse acknowledgedResponse=elasticsearchClient.indices().delete ( new DeleteIndexRequest ( "2021-01-10" ), RequestOptions.DEFAULT );
+	    
+	    System.out.println ( acknowledgedResponse.isFragment () );
+	}
+	//全量索引删除
+	@Test
+    public void test4() throws IOException {
+	    GetAliasesRequest request = new GetAliasesRequest();
+        GetAliasesResponse getAliasesResponse =  elasticsearchClient.indices().getAlias(request,RequestOptions.DEFAULT);
+        Map<String, Set<AliasMetadata>> map = getAliasesResponse.getAliases();
+        Set<String> indices = map.keySet();
+        for (String key : indices) {
+            AcknowledgedResponse acknowledgedResponse=elasticsearchClient.indices().delete ( new DeleteIndexRequest ( key ), RequestOptions.DEFAULT );
+            
+            System.out.println (key+":"+acknowledgedResponse.isFragment () );
+        }
+    }
 }
